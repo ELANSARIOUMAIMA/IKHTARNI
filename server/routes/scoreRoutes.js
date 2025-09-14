74dc5311963d8750be2dd76d204ac260c9d527e1
elansarioumaima <elansarioumaima93@gmail.com>
2025-09-14 17:14:22 +0100

    First deployment

--- server/routes/scoreRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { postScore } from "../controllers/scoreController.js";
-
-const router = express.Router();
-
-router.post("/", postScore);
-
-export default router;
--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ import 'dotenv/config'
 import connectDB from './config/db.js'
 import { clerkWebhooks } from './controllers/webhooks.js'
 import companyRoutes from './routes/companyRoutes.js'
-import scoreRoutes from "./routes/scoreRoutes.js";
+
 import connectCloudinary from './config/cloudinary.js'
 import jobRoutes from './routes/jobRoutes.js'
 import userRoutes from'./routes/userRoutes.js'
@@ -15,6 +15,20 @@ import {clerkMiddleware} from '@clerk/express'
 import dotenv from 'dotenv';
 import path from 'path';
 
+
+import 'dotenv/config';
+
+
+import bodyParser from 'body-parser';
+import fileUpload from 'express-fileupload';
+import { spawn } from 'child_process';
+
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+import fetch from 'node-fetch';
+import User from './models/User.js';
+import Job from './models/job.js';
+import JobAplication from './models/JobApplication.js';
 
 
  
@@ -39,6 +53,17 @@ app.use(clerkMiddleware())
 
 
 
+//
+app.use(cors());
+
+
+
+
+
+app.use(fileUpload());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 
 
 
@@ -58,12 +83,78 @@ app.post('/webhooks',clerkWebhooks)
 app.use('/api/company',companyRoutes)
 app.use('/api/jobs',jobRoutes)
 app.use('/api/users',userRoutes)
-app.use("/api/scores", scoreRoutes);
 
 
 
 
 
+
+
+
+
+// ES Modules __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+
+
+
+app.post("/scores", async (req, res) => {
+  try {
+   
+    const { userId, jobId } = req.body;
+    const job = await Job.findById(jobId);
+    const user = await User.findOne({ _id: userId });
+
+    if (!user || !user.resume) return res.status(400).json({ error: "No resume URL" });
+
+    // Télécharger le CV depuis Cloudinary
+    const response = await fetch(user.resume);
+    const buffer = await response.arrayBuffer();
+    const tmpPath = path.join(__dirname, 'temp_resume.pdf');
+    fs.writeFileSync(tmpPath, Buffer.from(buffer));
+
+
+    // Lancer le script Python
+    const scriptPath = path.join(__dirname, "../resume_matcher/resume_score.py");
+    const pythonPath = path.join(__dirname, '..', 'resume_matcher', 'venv', 'Scripts', 'python.exe');
+
+    console.log("Running Python script:");
+    console.log("Python path:", pythonPath);
+    console.log("Script path:", scriptPath);
+    console.log("Resume path:", tmpPath);
+    console.log("Job description:", job.description);
+
+    const pythonProcess = spawn(pythonPath, [scriptPath, tmpPath, job.description]);
+
+    let score = '0';
+    pythonProcess.stdout.on("data", data => score += data.toString());
+    pythonProcess.stderr.on("data", data => console.error("Python error:",data.toString()));
+
+    pythonProcess.on("close", async code => {
+      fs.unlinkSync(tmpPath); // supprimer le fichier temporaire
+
+      if (code === 0) {
+        score = parseFloat(score);
+
+        // Mettre à jour la collection JobApplication / applicant avec le score
+        await JobAplication.findOneAndUpdate(
+          { userId, jobId },
+          { score },
+          { new: true, upsert: true }
+        );
+
+        res.json({ success: true, score });
+      } else {
+        res.status(500).json({ success: false, message: "Error getting score" });
+      }
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
 
 
